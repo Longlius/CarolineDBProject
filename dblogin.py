414e222e865c40ee324f1edee732d1ffce489582
Matthew Longley <randomticktock@gmail.com>
2015-05-12 00:04:46 -0500

    fleshed out database login module as a class for reuse

--- dblogin.py
+++ dblogin.py
@@ -1,11 +1,49 @@
-# Function - getUserName
-# Description - asks the user to input their username to authenticate
-def getUserName():
-    global db_username
-    db_username = input('Enter your username: ')
+import getpass
+
+class DBLogin:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.hostaddr = ''
+        self.portnum = -1
+        
+    # Function - promptForUserName
+    # Description - asks the user to input their username to authenticate
+    def promptForUserName(self):
+        self.username = input('Enter your username: ')
     
-# Function - getPassword
-# Description - asks the user to input their password to authenticate
-def getPassword():
-    global db_password
-    db_password = getpass.getpass('Enter your password: ')
+    # Function - promptForPassword
+    # Description - asks the user to input their password to authenticate
+    def promptForPassword(self):
+        self.password = getpass.getpass('Enter your password: ')
+        
+    # Function - promptForAddr
+    # Description - asks the user to input the db's address
+    def promptForAddr(self):
+        self.hostaddr = input('Enter the address of the database: ')
+
+    # Function - promptForPort
+    # Description - asks the user to input the db's port number
+    def promptForPort(self):
+        while True:
+            p = int(input('Enter the port number to connect to: '))
+            if (p >= 0 and p <= 65535):
+                self.portnum = p
+                return
+            else:
+                print('Invalid entry. Port number must be between 0 and 65,535.')
+                
+    # Function - loginPrompt
+    # Description - prompts the user enter information to login
+    def loginPrompt(self):
+        self.promptForAddr()
+        self.promptForPort()
+        self.promptForUserName()
+        self.promptForPassword()
+                
+    def debugPrint(self):
+        print("Username: " + self.username)
+        print("Password: " + self.password)
+        print("DB address: " + self.hostaddr)
+        print("DB port: " + str(self.portnum))
+                
--- initdb.py
+++ initdb.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
-import dblogin, getpass, sys
+import dblogin, sys
 
 # Constants
 CONLANG_NAME = "gvwtokori"
 CONLANG_DB = CONLANG_NAME + "DB"
 
-# Global variables
-db_username = ''
-db_password = ''
-db_address = ''
-db_port = -1
-
-print(dblogin.__name__)
+x = dblogin.DBLogin()
+x.loginPrompt()
+print("------ DEBUG ------")
+x.debugPrint()
