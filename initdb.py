1bebf7ca2926b0554d25e744999274345ac1a5d1
Matthew Longley <randomticktock@gmail.com>
2015-05-11 20:19:17 -0500

    Added some constants

--- initdb.py
+++ initdb.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 
+import sys
+
+CONLANG_NAME = "gvwtokori"
+CONLANG_DB = CONLANG_NAME + "DB"
+
 print("Welp~")
+print("You are running Python version " + sys.version)
+print("The conlang you're using is: " + CONLANG_NAME)
+print("The conlang's DB is: " + CONLANG_DB)
