ba8f2dbf1420c61434d377cb08c4bb7ea2b9f130
Matthew Longley <randomticktock@gmail.com>
2015-05-11 21:07:25 -0500

    foo

--- initdb.py
+++ initdb.py
@@ -1,11 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import sys
+import getpass, sys
 
+# Constants
 CONLANG_NAME = "gvwtokori"
 CONLANG_DB = CONLANG_NAME + "DB"
 
-print("Welp~")
-print("You are running Python version " + sys.version)
-print("The conlang you're using is: " + CONLANG_NAME)
-print("The conlang's DB is: " + CONLANG_DB)
+# Global variables
+db_username = ''
+db_password = ''
+db_address = ''
+db_port = -1
+
+# Function - getUserName
+# Description - asks the user to input their username to authenticate
+def getUserName():
+    global db_username
+    db_username = input('Enter your username: ')
+    
+# Function - getPassword
+# Description - asks the user to input their password to authenticate
+def getPassword():
+    global db_password
+    db_password = getpass.getpass('Enter your password: ')
+
+print("Welp")
